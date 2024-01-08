0c3311e081b46cb92203e6a302298a74b58a41e5
simon3397 <simon3397@gmail.com>
2024-01-08 15:24:13 +0900

    [NEW]

---
+++ app/src/main/java/com/byurin/dice_roller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.byurin.dice_roller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
